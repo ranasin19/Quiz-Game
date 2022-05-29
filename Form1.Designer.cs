fcc3f2dccaf2263b30e5aaa449eee4b6d6efca6f
Rana omar <80989305+ranasin19@users.noreply.github.com>
2022-05-29 19:00:38 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace qgame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(168, 205);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(209, 23);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "أسئلة عشوائيه ممتعة";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(29, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.CheckAnswersEvents);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(296, 283);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.CheckAnswersEvents);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(29, 350);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 50);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.CheckAnswersEvents);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(296, 350);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(214, 50);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.CheckAnswersEvents);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::qgame.Properties.Resources.space;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(455, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::qgame.Properties.Resources.client_bg;
+            this.ClientSize = new System.Drawing.Size(540, 457);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qgame
+{
+    public partial class Form1 : Form
+    {
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = 8;
+        }
+        private void CheckAnswersEvents(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+            }
+
+            if (questionNumber == totalQuestions)
+            {
+
+
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+                    "!اللعبة انتهت" + Environment.NewLine +
+                    "لقد أجبت على "  + score +  " اسئلة." + Environment.NewLine +
+                   "OK للعب مجددا اضغط"
+                    );
+
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+
+            }
+
+            questionNumber++;
+            askQuestion(questionNumber);
+
+        }
+        private void askQuestion(int qnum)
+        {
+
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.eygpt;
+
+                    lblQuestion.Text = "ماهي عاصمة مصر؟";
+
+                    button1.Text = "بيروت";
+                    button2.Text = "القاهرة";
+                    button3.Text = "الرياض";
+                    button4.Text = "الكويت";
+
+                    correctAnswer = 2;
+
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.nhrt;
+
+                    lblQuestion.Text = "ماهو اطول نهر بالعالم؟";
+
+                    button1.Text = "نهر النيل";
+                    button2.Text = "نهر الارن";
+                    button3.Text = "نهر دجلة";
+                    button4.Text = "نهر المسيسي";
+
+                    correctAnswer = 1;
+
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.fortnite;
+
+                    lblQuestion.Text = "ما اسم هذه اللعبة؟";
+
+                    button1.Text = "Call of Duty";
+                    button2.Text = "Battlefield";
+                    button3.Text = "Fortnite";
+                    button4.Text = "CS - GO";
+
+                    correctAnswer = 3;
+
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.abs;
+
+                    lblQuestion.Text = "ماهو عدد العضلات في جسم الانسان؟";
+
+                    button1.Text = "عضلة";
+                    button2.Text = "1000";
+                    button3.Text = "111";
+                    button4.Text = "620";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.space;
+
+                    lblQuestion.Text = "من هو اول رائد فضاء سعودي؟";
+
+                    button1.Text = "الأمير سلطان بس سلمان";
+                    button2.Text = "الأمير خالد بن فيصل";
+                    button3.Text = "الوليد بن طلال";
+                    button4.Text = "أحمد الصانع";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 6:
+                    pictureBox1.Image = Properties.Resources.coding;
+
+                    lblQuestion.Text = "اي من التالي تعتبر من لغات البرمجة؟";
+
+                    button1.Text = "C#";
+                    button2.Text = "paython";
+                    button3.Text = "java";
+                    button4.Text = "جميع ما سبق";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 7:
+                    pictureBox1.Image = Properties.Resources.sea;
+
+                    lblQuestion.Text = "ماهو اخطر الحيوانات البحرية؟";
+
+                    button1.Text = "الاخطبوط";
+                    button2.Text = "القرش";
+                    button3.Text = "الحوت الازرق";
+                    button4.Text = "حصان البحر";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 8:
+                    pictureBox1.Image = Properties.Resources.aus;
+
+                    lblQuestion.Text = "ماهي عاصمة استراليا؟";
+
+                    button1.Text = "Birmingham";
+                    button2.Text = "Brighton";
+                    button3.Text = "Liverpool";
+                    button4.Text = "Canberra";
+
+                    correctAnswer = 4;
+
+                    break;
+            }
+            }
+    }
+}
